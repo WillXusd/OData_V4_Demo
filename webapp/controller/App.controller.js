9805fce3c06d5172bb903c2b1cc896b5e73e9bd8
Will Xu <xusidong0819@163.com>
2018-08-20 05:50:50 +0000

    Data Access

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function(Controller,JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
+], function(Controller,JSONModel,MessageToast,MessageBox) {
 	"use strict";
 
 	return Controller.extend("OData_V4.controller.App", {
@@ -11,6 +13,21 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oJSONData);
 			this.getView().setModel(oModel, "appView");
+		},
+		//refresh data
+		onRefresh : function() {
+			var oBinding = this.byId("peopleList").getBinding("items");
+			
+			if(oBinding.hasPendingChanges()){
+				MessageBox.error(this._getText("refreshNotPossibleMessage"));
+				return;
+			}
+			oBinding.refresh();
+			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+		
+		_getText:function(sTextId,aArgs){
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId,aArgs);
 		}
 	});
 });
