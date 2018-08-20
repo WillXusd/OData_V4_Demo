1a09cb8396ec824c9bf0f87fbebca1b2447356c7
Will Xu <xusidong0819@163.com>
2018-08-20 07:43:22 +0000

    sorter and filter

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,14 +2,19 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast",
-	"sap/m/MessageBox"
-], function(Controller,JSONModel,MessageToast,MessageBox) {
+	"sap/m/MessageBox",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/FilterType"
+], function(Controller,JSONModel,MessageToast,MessageBox,Sorter, Filter, FilterOperator, FilterType) {
 	"use strict";
 
 	return Controller.extend("OData_V4.controller.App", {
 	onInit : function () {
 			var oJSONData = {
-				busy : false
+				busy : false,
+				order: 0
 			};
 			var oModel = new JSONModel(oJSONData);
 			this.getView().setModel(oModel, "appView");
@@ -24,6 +29,38 @@ sap.ui.define([
 			}
 			oBinding.refresh();
 			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+
+		onSearch : function () {
+			var oView = this.getView(),
+				sValue = oView.byId("searchField").getValue(),
+				oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
+		
+			oView.byId("peopleList").getBinding("items").filter(oFilter, FilterType.Application);
+		},
+
+		onSort : function () {
+			var oView = this.getView(),
+				aStates = [undefined, "asc", "desc"],
+				aStateTextIds = ["sortNone", "sortAscending", "sortDescending"],
+				sMessage,
+				iOrder = oView.getModel("appView").getProperty("/order");
+			
+			
+			console.log("oView->",oView);
+			console.log("aStates->",aStates);
+			console.log("iOrder->",iOrder);
+			
+			iOrder = (iOrder + 1) % aStates.length;  
+			console.log("iOrder2->",iOrder);
+			var sOrder = aStates[iOrder];
+
+			oView.getModel("appView").setProperty("/order", iOrder);
+			
+			oView.byId("peopleList").getBinding("items").sort(sOrder && new Sorter("LastName", sOrder === "desc"));
+
+			sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+			MessageToast.show(sMessage);
 		},
 		
 		_getText:function(sTextId,aArgs){
